bd58d4887fe18a0a5934a629606386d83d4904ba
Lion-Hunter <01.08.2000>
2019-04-30 12:07:36 +0300

    Deleted some trash

--- src/main/java/Transposer.java
+++ src/main/java/Transposer.java
@@ -6,17 +6,16 @@ public class Transposer {
     public static void transposer(String input, String output, int num, boolean cut, boolean align) throws IOException {
 
         BufferedReader buf;
-        BufferedReader bufR = null;
 
         if (input == null) { // make up inputFile from console strings for empty value of input
             InputStreamReader reader = new InputStreamReader(System.in);
             buf = new BufferedReader(reader);
-            bufR = new BufferedReader(reader);
         } else buf = new BufferedReader(new FileReader(input));
 
         String line = buf.readLine();
 
-        ArrayList<String> transposedStrings = new ArrayList<String>(); // array, which stores transposed strings
+        ArrayList<StringBuilder> transposedStrings = new ArrayList<StringBuilder>(); // array, which stores transposed strings
+        ArrayList<String> finish = new ArrayList<String>();
 
         while (line != null) {
             String[] strings = line.split(" +"); //transform current string to the array of strings
@@ -24,7 +23,7 @@ public class Transposer {
             for (int i = 0; i < strings.length; i++) {
                 if (cut && strings[i].length() > num) strings[i] = strings[i].substring(0, num);
                 StringBuilder result = new StringBuilder();
-                if (i > transposedStrings.size() - 1) transposedStrings.add("");
+                if (i > transposedStrings.size() - 1) transposedStrings.add(new StringBuilder());
 
                 if (!align && strings[i].length() < num) {
                     result.append(strings[i]);  // left alignment
@@ -34,19 +33,30 @@ public class Transposer {
                     result.append(strings[i]);
                 }
 
-                transposedStrings.set(i, transposedStrings.get(i) + result); // add string to result array
+                transposedStrings.get(i).append(result); // add string to result array
             }
 
             line = buf.readLine();
+
+
         }
+        for (StringBuilder str: transposedStrings) finish.add(str.toString());
         buf.close();
 
+        Writer writer;
+
         if (output != null) { //write final strings into output file
-            FileWriter wr = new FileWriter(output);
-            for (String string: transposedStrings) if (!string.equals(transposedStrings.get(transposedStrings.size() - 1))) {
-                wr.write(string + "\n");
-            } else wr.write(string);
-            wr.close();
-        } else for (String string: transposedStrings) System.out.println(string); // write final strings into console
+            writer = new FileWriter(output);
+
+            for (String string: finish) if (!string.equals(transposedStrings.get(transposedStrings.size() - 1))) {
+                writer.write(string + "\n");
+            } else writer.write(string);
+
+            writer.close();
+        } else {
+            writer = new StringWriter();
+
+            for (String string: finish) writer.write(string); // write final strings into console
+        }
     }
 }
--- src/test/java/TransposeTest.java
+++ src/test/java/TransposeTest.java
@@ -6,25 +6,25 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class TransposeTest {
-    public static boolean test(String input, String output, String test, int num, boolean cut, boolean align) throws IOException {
+    public static boolean test(String input, String output, String test, int num,
+                               boolean cut, boolean align) throws IOException {
         Transposer.transposer(input, output, num, cut, align);
+
         BufferedReader buf1 = new BufferedReader(new FileReader(output));
         BufferedReader buf2 = new BufferedReader(new FileReader(test));
         String outLine = buf1.readLine();
         String testLine = buf2.readLine();
-        boolean result = true;
 
         while (outLine != null || testLine != null) {
             if (outLine == null || !outLine.equals(testLine)) {
-                result = false;
-                break;
+                return false;
             }
 
             outLine = buf1.readLine();
             testLine = buf2.readLine();
         }
 
-        return result;
+        return true;
     }
 
     @Test
@@ -43,5 +43,11 @@ public class TransposeTest {
     public void alignTest() throws IOException {
         Assert.assertTrue(test("InputFiles/third.txt", "output3.txt",
                 "TestFiles/test3.txt", 5, false, true));
+    }
+
+    @Test
+    public void bothTest() throws IOException {
+        Assert.assertTrue(test("InputFiles/fourth.txt", "output4.txt",
+                "TestFiles/test4.txt", 4, true, true));
     }
 }
